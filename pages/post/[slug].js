2ce9e347f1462750bc6bebc9b1a86e4cd947694f
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-20 14:54:05 -0700

    get details data query

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,15 +1,15 @@
 import React from 'react'
 import {getPost,getPostDetails} from '../../services'
 import {PostDetail,Category,PostWidget,Author,Comments,CommentsForm} from '../../components'
-const PostDetails = () => {
+const PostDetails = ({post}) => {
   return (
     <div className='container mx-auto px-10'>
          <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
             <div className='col-span-1 lg:col-span-8'>
-            <PostDetail/>
-            <Author/>
-            <CommentsForm/>
-            <Comments/>      
+            <PostDetail post={post}/>
+            <Author author={post.author}/>
+            <CommentsForm slug={post.slug}/>
+            <Comments slug={post.slug}/>      
             </div>
             <div className='col-span-1 lg:col-span-4'>
                 <div className='relative lg:sticky top-8'>
@@ -24,3 +24,11 @@ const PostDetails = () => {
 }
 
 export default PostDetails
+
+export async function getStaticProps({params})
+{
+  const data=await getPostDetails(params.slug)
+  return {
+    props:{post:data}
+  }
+}
