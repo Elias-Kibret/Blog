6617abebb19818caeccaba9ae2dd7d7f93e0060d
Elias-Kibret <eliaskibret2013n2@gmail.com>
2022-08-24 02:29:42 -0700

    slider

--- components/FeaturedPostCard.jsx
+++ components/FeaturedPostCard.jsx
@@ -7,15 +7,15 @@ const FeaturedPostCard = ({ post }) => (
 <div className=' mb-8'>
 <Link href={`post/${post.slug}`}>
                  
-                 <div className='overflow-hidden max-w-[270px] border border-gray-100 rounded-lg mx-4 h-[150px] relative object-cover'>
+                 <div className='overflow-hidden max-w-[270px] border border-gray-100 rounded-lg mx-4 h-170px relative object-cover'>
                      <img
                         src={post.featuredImage.url}
-                        width="250px"
+                        width="270px"
                         height="170px"
-                        className='rounded-lg shadow-lg overflow-hidden object-cover'
+                        className='rounded-lg shadow-lg overflow-hidden object-contain'
                         />   
              
-                        <div className=' px-3 py-5 bg-black absolute htop-50 bottom-0 right-0 left-0 bg-opacity-40 max-h-[100px} '>
+                        <div className=' px-3 py-5 bg-black absolute htop-50 bottom-0 right-0 left-0 bg-opacity-40 max-h-100px '>
                          <div className='flex items-center'>
              
                               <img 
